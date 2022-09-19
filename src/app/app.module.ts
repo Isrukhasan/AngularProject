a092a13b3d98f90131c8d2c8284d4cb3eb9894b2
Isruk <isrukhasan@gmail.com>
2022-09-20 00:33:10 +0600

    FC

---
+++ src/app/app.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { AppComponent } from './app.component';
+import { ProductListComponent } from './products/product-list.component';
+import { ConvertToSpacesPipe } from './shared/convert-to-spaces.pipe';
+
+@NgModule({
+  declarations: [AppComponent, ProductListComponent, ConvertToSpacesPipe],
+  imports: [BrowserModule, NgbModule, FormsModule],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
